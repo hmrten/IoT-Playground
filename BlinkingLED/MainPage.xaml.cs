ccd990b0e899f792acd5fa0eab2239b0f8f77d7c
Mårten Hansson <hmrten@gmail.com>
2016-04-15 14:20:11 +0200

    add blinking led and buton input examples

---
+++ BlinkingLED/MainPage.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Devices.Gpio;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace BlinkingLED
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        private const int PIN_LED = 5;
+
+        private GpioController gpio;
+        private GpioPin pin;
+        private GpioPinValue pinValue;
+        private DispatcherTimer timer;
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+            gpio = GpioController.GetDefault();
+            pin = gpio.OpenPin(PIN_LED);
+            pin.Write(GpioPinValue.High);
+            pin.SetDriveMode(GpioPinDriveMode.Output);
+            pinValue = GpioPinValue.High;
+
+            timer = new DispatcherTimer();
+            timer.Interval = TimeSpan.FromMilliseconds(500);
+            timer.Tick += Timer_Tick;
+            timer.Start();
+        }
+
+        private void Timer_Tick(object sender, object e)
+        {
+            pinValue = pinValue == GpioPinValue.High ? GpioPinValue.Low : GpioPinValue.High;
+            pin.Write(pinValue);
+        }
+    }
+}
---
+++ ButtonInput/MainPage.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Devices.Gpio;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace ButtonInput
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        private const int PIN_BUTTON = 6;
+
+        private GpioController gpio;
+        private GpioPin pin;
+        private int hits;
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+            gpio = GpioController.GetDefault();
+
+            pin = gpio.OpenPin(PIN_BUTTON);
+
+            // When button is connected to a PIN -> GND configure mode to InputPullUp
+            pin.SetDriveMode(GpioPinDriveMode.InputPullUp);
+            
+            // How often you can press the button
+            pin.DebounceTimeout = TimeSpan.FromMilliseconds(1);
+
+            // Callback to run when value changes
+            pin.ValueChanged += Button_Changed;
+        }
+
+        private async void Button_Changed(GpioPin sender, GpioPinValueChangedEventArgs args)
+        {
+            await Dispatcher.RunAsync(Windows.UI.Core.CoreDispatcherPriority.Normal, () =>
+            {
+                // When button closes the circuit
+                if (args.Edge.CompareTo(GpioPinEdge.FallingEdge) == 0)
+                {
+                    ++hits;
+                    textBlock.Text = "Hits: " + hits;
+                }
+            });
+        }
+    }
+}
